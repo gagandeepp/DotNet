3edc7162cbcf3e0b9306cc40ab76fdb8509d767a
gagand <gagand@exzeoindia.com>
2017-02-02 12:31:07 +0530

    First Commit

---
+++ DelegateDemo.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DelegateDemo
+{
+    class Program
+    {
+        //Simple Delegate
+        public delegate void SayHello(string name);
+        public delegate int Add(int a, int b);
+        //public Func<int, int> Square();
+        public static void HelloFunction(string input)
+        {
+            Console.WriteLine("Hello:" + input);
+        }
+        public static void HiFunction(string input)
+        {
+            Console.WriteLine("Hi:" + input);
+        }
+
+        static void Main(string[] args)
+        {
+            //Named Delegate
+            SayHello obj = HelloFunction;
+            obj.Invoke("Gagan");
+            //Anonymous Delegate
+            Add f1 = delegate(int a, int b)
+            {
+                return a + b;
+            };
+            f1.Invoke(4, 3);
+            //Composable Delegate
+            obj += HiFunction;    //When Line 26 is commented then first Hello Gagan is printed then Hi Gagan is printed
+            obj.Invoke("Gagan");
+            //Func Delegate -
+            Func<int, int> Square = (input) => { return input * input; };
+            Console.WriteLine(string.Format("This is output of Func Delegate:" + Square(4)));
+            //Action Delegate - No return Type 
+            Action<double, double> Power = (input1, input2) => { Console.WriteLine("Action Delegate:"+Math.Pow(input1, input2)); };
+            Power(2, 3);
+            //Predicate Delegate - Bool return type
+            Predicate<int> IsGreateThenZero = (input) => { return input > 0 ? true : false; };
+            Console.WriteLine(IsGreateThenZero(5));
+            //Comparison Delegate
+        }
+    }
+}
---
+++ Polymorphism.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace sample
+{
+    //class BaseClass       version 1
+    //{
+    //    public void SayHello()
+    //    {
+    //        Console.WriteLine("SayHello in Base");
+    //    }
+    //    public void SayHi()
+    //    {
+    //        Console.WriteLine("SayHi in Base");
+    //    }
+    //}
+    //class DerivedClass : BaseClass
+    //{
+    //    public void SayHello()
+    //    {
+    //        Console.WriteLine("SayHello in Derived");        //BaseClass obj2 = new DerivedClass();      and same for (method is virtual/non virtual in base class and method used as new in Derived class)
+                                                               // obj2.SayHello();  //Say Hello in Base
+                                                               //obj2.SayHi();  //Say Hi in Base
+    //    }
+    //    public void SayHi()
+    //    {
+    //        Console.WriteLine("SayHi in Derived");
+    //    }
+    //}
+
+
+    //class BaseClass  Version 2
+    //{
+    //    public virtual void SayHello()
+    //    {
+    //        Console.WriteLine("SayHello in Base");
+    //    }
+    //    public virtual void SayHi()
+    //    {
+    //        Console.WriteLine("SayHi in Base");
+    //    }
+    //}
+    //class DerivedClass : BaseClass
+    //{
+    //    public override void SayHello()
+    //    {
+    //        Console.WriteLine("SayHello in Derived");                          //BaseClass obj2 = new DerivedClass();
+                                                                                // obj2.SayHello();  //Say Hello in Derived
+                                                                                //obj2.SayHi();  //Say Hi in Dervied
+    //    }
+    //    public override void SayHi()
+    //    {
+    //        Console.WriteLine("SayHi in Derived");
+    //    }
+    //}
+
+    // abstract class BaseClass
+    //{
+    //    public abstract void SayHello();
+      
+    //}
+    //class DerivedClass : BaseClass
+    //{
+    //    public virtual void SayHello()
+    //    {
+    //        Console.WriteLine("SayHello in Derived");
+    //    }
+    //}
+
+    interface IHello
+    {
+        void Hello();
+    }
+
+    interface ISayHello
+    {
+        void Hello();
+    }
+
+    class SayHello : IHello,ISayHello
+    {
+        void IHello.Hello()
+        {
+            Console.WriteLine("From IHello");
+        }
+        void ISayHello.Hello()
+        {
+            Console.WriteLine("From ISayHello");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            SayHello obj = new SayHello();
+            IHello ih = obj;
+            ih.Hello();                    // IHello
+            ISayHello ish = (ISayHello)ih; // or obj 
+            ish.Hello();                   // ISayHello
+        }
+
+    }
+
+}
