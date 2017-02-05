146d36fdd3e95327b12441b76e6a0703ec17b0bb
gagandeep531 <gagandeep531@gmail.com>
2017-02-05 18:06:06 +0530

    Related to HttpModules and HAndlers

---
+++ SimpleModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web;
+public class SimpleModule : IHttpModule
+{
+    private HttpApplication _application = null;
+    public void Dispose()
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Init(HttpApplication context)
+    {
+        _application = context;
+        context.EndRequest += context_EndRequest;
+    }
+    void context_EndRequest(object sender, EventArgs e)
+    {
+        string message = string.Format("processed on {0}",
+        System.DateTime.Now.ToString());
+        _application.Context.Response.Write(message);
+    }
+}
